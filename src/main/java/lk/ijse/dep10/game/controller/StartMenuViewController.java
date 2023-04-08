4ada130d44efb8eadf4855d7157cea1da013066d
Nuwan-Wimaladharma <nuwanwd20@gmail.com>
2023-04-08 12:45:44 +0530

    Start Menu UI completed

---
+++ src/main/java/lk/ijse/dep10/game/controller/StartMenuViewController.java
@@ -0,0 +1,38 @@
+package lk.ijse.dep10.game.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class StartMenuViewController {
+
+    @FXML
+    private Button btnQuitGame;
+
+    @FXML
+    private Button btnStartGame;
+
+    @FXML
+    private Button btnViewHighScores;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void btnQuitGameOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnStartGameOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnViewHighScoresOnAction(ActionEvent event) {
+
+    }
+
+}
+
